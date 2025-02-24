5fc4fd0c8118e67279b139ba4c9088ebdf9cc917
titem <etienne.guillemin@pm.me>
2025-02-24 02:26:30 +0100

    font

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,32 +13,32 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.wrap = false
 
 -- Appearance
-vim.o.guifont = "JetBrainsMono Nerd Font Mono:h10,Noto Color Emoji"
+vim.o.guifont = "Iosevka:h12,Noto Color Emoji"
 vim.cmd [[colorscheme sarnai]]
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Terminal
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
 })
 
 vim.keymap.set("n", "<leader>tt", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
 end
 )
 
