3725a4cb02304794be587c325076158974dbec64
titembaatar <etienne.guillemin@protonmail.com>
2025-03-05 02:49:55 +0100

    nvim: sarnai github instead of local

--- nvim/.config/nvim/lua/plugins/sarnai.lua
+++ nvim/.config/nvim/lua/plugins/sarnai.lua
@@ -1,6 +1,6 @@
 return {
-  -- "titembaatar/sarnai.nvim",
-  dir = "/home/titem/code/sarnai.nvim",
+  "titembaatar/sarnai.nvim",
+  -- dir = "/home/titem/code/sarnai.nvim",
   lazy = false,
   priority = 1000,
   opts = {
