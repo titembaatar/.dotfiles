741e23b598b76565dbd1f5c193da6c94de889d88
titembaatar <etienne.guillemin@protonmail.com>
2025-03-11 04:24:41 +0100

    nvim: python lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,59 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "saghen/blink.cmp",
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require("lspconfig").bashls.setup { capabilities = capabilities }
-      require("lspconfig").docker_compose_language_service.setup { capabilities = capabilities }
-      require("lspconfig").dockerls.setup { capabilities = capabilities }
-      require("lspconfig").gopls.setup { capabilities = capabilities }
-      require("lspconfig").lua_ls.setup { capabilities = capabilities }
-      require("lspconfig").marksman.setup { capabilities = capabilities }
-      require("lspconfig").yamlls.setup { capabilities = capabilities }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local capabilities = require('blink.cmp').get_lsp_capabilities()
+			require("lspconfig").bashls.setup { capabilities = capabilities }
+			require("lspconfig").docker_compose_language_service.setup { capabilities = capabilities }
+			require("lspconfig").dockerls.setup { capabilities = capabilities }
+			require("lspconfig").gopls.setup { capabilities = capabilities }
+			require("lspconfig").lua_ls.setup { capabilities = capabilities }
+			require("lspconfig").marksman.setup { capabilities = capabilities }
+			require("lspconfig").yamlls.setup { capabilities = capabilities }
+			require("lspconfig").pylsp.setup {
+				capabilities = capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = { enabled = true },
+							pyls_black = { enabled = true },
+							isort = { enabled = true },
+							mypy = { enabled = true },
+						},
+					},
+				},
+			}
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then return end
 
-          if client.supports_method('textDocument/formatting') then
-            -- Format current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  }
+					if client.supports_method('textDocument/formatting') then
+						-- Format current buffer on save
+						vim.api.nvim_create_autocmd('BufWritePre', {
+							buffer = args.buf,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	}
 }
