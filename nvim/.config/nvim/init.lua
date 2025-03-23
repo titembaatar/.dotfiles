81375a494189d8abb1be5f8d71f77136b98cd6b3
titembaatar <etienne.guillemin@protonmail.com>
2025-03-23 19:03:51 +0100

    nvim: order matters

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
-require("config.colorscheme") -- Color scheme configuration
+require("config.options")     -- General Neovim options
 require("config.keymaps")     -- Key mappings
 require("config.lazy")        -- Plugin manager
-require("config.options")     -- General Neovim options
+require("config.colorscheme") -- Color scheme configuration
