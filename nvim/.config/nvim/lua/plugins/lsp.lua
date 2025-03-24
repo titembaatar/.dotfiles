090104330e0b66269fc51582ae869160df699301
titembaatar <etienne.guillemin@protonmail.com>
2025-03-24 18:06:51 +0100

    nvim: lsp, witch to blink.cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,20 @@
 return {
   {
     "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
     opts = {
       ensure_installed = {
         -- LSP
-        "lua-language-server",
-        "pyright",
+        "bashls",
+        "cssls",
+        "dockerls",
         "gopls",
-        "bash-language-server",
-        "yaml-language-server",
-        "json-lsp",
+        "html",
+        "jsonls",
+        "lua_ls",
         "marksman",
+        "pyright",
+        "harper_ls",
+        "yamlls",
 
         -- Linters
         "shellcheck",
@@ -29,10 +31,7 @@ return {
 
   {
     "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
+    dependencies = { "williamboman/mason.nvim" },
     opts = {
       automatic_installation = true,
     },
@@ -44,23 +43,26 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
+      "saghen/blink.cmp",
       "folke/lazydev.nvim",
     },
     opts = {
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          prefix = "●",
-        },
-        severity_sort = true,
-      },
-      -- Add common capabilities for LSP servers
       capabilities = {},
       -- Default LSP server configuration
       servers = {
+        bashls = {
+          filetypes = { "sh", "bash" },
+          settings = {
+            bashIde = {
+              shellcheckPath = "shellcheck",
+            },
+          },
+        },
+        cssls = {},
+        dockerls = {},
+        gopls = {},
+        html = {},
+        jsonls = {},
         lua_ls = {
           settings = {
             Lua = {
@@ -79,25 +81,16 @@ return {
             },
           },
         },
-        pyright = {},
-        gopls = {},
-        bashls = {
-          filetypes = { "sh", "bash" },
-          settings = {
-            bashIde = {
-              shellcheckPath = "shellcheck",
-            },
-          },
-        },
-        jsonls = {},
-        yamlls = {},
         marksman = {},
+        pyright = {},
+        harper_ls = {},
+        yamlls = {},
       },
     },
     config = function(_, opts)
-      local lspconfig = require("lspconfig")
       require("lazydev").setup()
 
+      local lspconfig = require("lspconfig")
       local null_ls = require("null-ls")
 
       null_ls.setup({
@@ -112,21 +105,17 @@ return {
       -- Configure diagnostics
       vim.diagnostic.config(opts.diagnostics)
 
-      -- Setup autoformat on save
+      -- Setup auto-format on save
       vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function()
           vim.lsp.buf.format({ timeout_ms = 2000 })
         end,
       })
 
-      -- Setup capabilities with nvim-cmp
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
       -- Setup servers
-      for server, server_opts in pairs(opts.servers) do
-        server_opts.capabilities = capabilities
-        lspconfig[server].setup(server_opts)
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
       end
     end,
   },
@@ -134,80 +123,32 @@ return {
   {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
-    dependencies = { "mason.nvim" },
+    dependencies = { "williamboman/mason.nvim" },
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      -- Load minimal snippets for common languages
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- Confirm selection
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-          -- Navigate between items
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-
-          -- Scroll documentation
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-          -- Abort the completion
-          ["<C-e>"] = cmp.mapping.abort(),
-
-          -- Navigate in snippets if active
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
-
-  -- Lua development for Neovim
   {
     "folke/lazydev.nvim",
-    lazy = true,
-    opts = {},
+    ft = "lua", -- only load on Lua files
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
   },
 }
