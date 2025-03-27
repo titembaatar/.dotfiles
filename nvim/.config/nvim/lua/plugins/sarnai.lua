d74a54a0a751b1b5639f02a365949b791a522caa
titembaatar <etienne.guillemin@protonmail.com>
2025-03-27 02:54:15 +0100

    nvim: back to sarnai

---
+++ nvim/.config/nvim/lua/plugins/sarnai.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    -- "titembaatar/sarnai.nvim",
+    dir = "~/code/sarnai.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      cache = false,
+      styles = {
+        italic = false,
+        bold = false,
+        underline = false,
+      }
+    },
+  },
+}
