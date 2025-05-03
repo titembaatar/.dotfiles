316242da90f40a59a7107f02892f6064ba3eb670
titembaatar <etienne.guillemin@protonmail.com>
2025-05-03 13:31:22 +0200

    nvim: cleaner mini plugins

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -4,52 +4,28 @@ return {
     version = false,
     event = "VeryLazy",
     config = function()
-      -- Text editing --
-      -- mini.ai
       require("mini.ai").setup({})
+      require("mini.bracketed").setup({})
+      require("mini.bufremove").setup({})
+      require("mini.cursorword").setup({})
+      require("mini.icons").setup({})
+      require("mini.pairs").setup({})
+      require('mini.sessions').setup({})
+      require("mini.tabline").setup({})
+      require("mini.trailspace").setup({})
 
-      -- mini.comment
       require("mini.comment").setup({
         options = {
           ignore_blank_line = true,
         },
       })
 
-      -- mini.pairs
-      require("mini.pairs").setup({})
-
-      -- mini.surround
-      require("mini.surround").setup({
-        mappings = {
-          add = "gsa",
-          delete = "gsd",
-          find = "gsf",
-          find_left = "gsF",
-          highlight = "gsh",
-          replace = "gsr",
-          update_n_lines = "gsn",
-        },
-      })
-
-      -- General workflow --
-      -- mini.bracketed
-      require("mini.bracketed").setup({})
-
-      -- mini.bufremove
-      require("mini.bufremove").setup({})
-
-      -- mini.files
       require("mini.files").setup({
         options = {
           show_hidden = true,
         },
       })
 
-      -- Appearance --
-      -- mini.cursorword
-      require("mini.cursorword").setup({})
-
-      -- mini.hipatterns
       require("mini.hipatterns").setup({
         highlighters = {
           todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
@@ -60,10 +36,6 @@ return {
         },
       })
 
-      -- mini.icons
-      require("mini.icons").setup({})
-
-      -- mini.indentscope
       require("mini.indentscope").setup({
         draw = {
           animation = function()
@@ -79,23 +51,21 @@ return {
         symbol = "│",
       })
 
-      -- mini.statusline
       require("mini.statusline").setup({
         use_icons = true,
       })
 
-      -- mini.tabline
-      require("mini.tabline").setup({})
-
-      -- mini.trailspace
-      require("mini.trailspace").setup({})
-      -- -- Auto-trim whitespace on save
-      -- vim.api.nvim_create_autocmd("BufWritePre", {
-      --   pattern = "*",
-      --   callback = function()
-      --     require("mini.trailspace").trim()
-      --   end,
-      -- })
+      require("mini.surround").setup({
+        mappings = {
+          add = "gsa",
+          delete = "gsd",
+          find = "gsf",
+          find_left = "gsF",
+          highlight = "gsh",
+          replace = "gsr",
+          update_n_lines = "gsn",
+        },
+      })
     end,
   },
 }
