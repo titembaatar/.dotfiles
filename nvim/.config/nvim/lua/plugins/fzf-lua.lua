e467ce4c0bdce346ff5bb1ace0baf1fee4086875
titembaatar <etienne.guillemin@protonmail.com>
2025-05-03 13:30:24 +0200

    nvim: del unused plugins

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "ibhagwan/fzf-lua",
-    enabled = false,
-    dependencies = { "echasnovski/mini.icons" },
-  }
-}
--- nvim/.config/nvim/lua/plugins/persistence.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "folke/persistence.nvim",
-    enabled = false,
-    event = "BufReadPre",
-  }
-}
