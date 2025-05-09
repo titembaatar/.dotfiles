6a81cbf53cc9dbcf422059edcf1d17be3312a249
titembaatar <etienne.guillemin@protonmail.com>
2025-05-10 00:40:02 +0200

    waybar: rice + homelab healthcheck script

---
+++ waybar/.config/waybar/scripts/homelab.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NODES=("10.0.0.10" "10.0.0.11" "10.0.0.12")
+NODE_NAMES=("mukhulai" "borchi" "borokhul")
+TIMEOUT=2
+HEALTHY=true
+TOOLTIP=""
+
+for i in "${!NODES[@]}"; do
+  TOOLTIP+="${NODE_NAMES[$i]}: "
+
+  if ping -c 1 -W $TIMEOUT "${NODES[$i]}" > /dev/null 2>&1; then
+    TOOLTIP+="up"
+  else
+    TOOLTIP+="down"
+    HEALTHY=false
+  fi
+
+  if [ "$i" -lt $((${#NODES[@]} - 1)) ]; then
+    TOOLTIP+="\n"
+  fi
+done
+
+if $HEALTHY; then
+  TEXT="up"
+  CLASS="up"
+else
+  TEXT="down"
+  CLASS="down"
+fi
+
+echo "{\"text\": \"$TEXT\", \"class\": \"$CLASS\", \"tooltip\": \"$TOOLTIP\"}"
