da9c52d0f36c30f88f537c86a338f9d19663f360
titembaatar <etienne.guillemin@protonmail.com>
2025-05-10 01:31:02 +0200

    Remove Neovim config directory to prepare for submodule

--- env/.config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-_G.Util = require("util")
-require("config.options")     -- General Neovim options
-require("config.keymaps")     -- Key mappings
-require("config.lazy")        -- Plugin manager
-require("config.colorscheme") -- Color scheme configuration
