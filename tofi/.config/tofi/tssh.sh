51f8a7a3e5dbb637c46ac2fea5b9e78e2878a290
titembaatar <etienne.guillemin@protonmail.com>
2025-05-10 00:39:20 +0200

    tofi: ssh into tmux ssh session

--- tofi/.config/tofi/tssh.sh
+++ tofi/.config/tofi/tssh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-TERM=$(grep 'set $term' "$HOME"/.config/sway/config | awk '{print $3}')
 TOFI_CONFIG=$HOME/.config/tofi/config
 
 ssh_list=$(grep '^Host ' "$HOME"/.ssh/config | awk '{print $2}')
 selected=$(echo "$ssh_list" | tofi --config "$TOFI_CONFIG")
-$TERM -e ssh "$selected"
+tmux new-window -d -t ssh: -n "$selected" ssh "$selected"; tmux switch-client -t ssh:"$selected"
+swaymsg workspace 1
