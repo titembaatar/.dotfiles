46a72d618960f68c8593b88112bffa3a83e54797
titembaatar <etienne.guillemin@protonmail.com>
2025-05-16 02:46:05 +0200

    tofi: ssh with theprimeagen/tmux-sessionizer

--- env/.config/tofi/ssh.sh
+++ env/.config/tofi/ssh.sh
@@ -5,5 +5,5 @@ TOFI_CONFIG=$HOME/.config/tofi/config
 
 ssh_list=$(grep '^Host ' "$HOME"/.ssh/config | awk '{print $2}')
 selected=$(echo "$ssh_list" | tofi --config "$TOFI_CONFIG")
-tmux new-window -d -t ssh: -n "$selected" ssh "$selected"; tmux switch-client -t ssh:"$selected"
+tmux new-session -ds "$selected" -n ssh ssh "$selected"; tmux switch-client -t "$selected"
 swaymsg workspace 1
