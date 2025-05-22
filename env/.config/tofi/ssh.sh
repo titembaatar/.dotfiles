5142e1cd1883d92387447863c8ee320f2a0c46da
titembaatar <etienne.guillemin@protonmail.com>
2025-05-22 19:11:17 +0200

    move ssh to sesionniser

--- env/.config/tofi/ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-TOFI_CONFIG=$HOME/.config/tofi/config
-
-ssh_list=$(grep '^Host ' "$HOME"/.ssh/config | awk '{print $2}')
-selected=$(echo "$ssh_list" | tofi --config "$TOFI_CONFIG")
-tmux new-session -ds "$selected" -n ssh ssh "$selected"; tmux switch-client -t "$selected"
-swaymsg workspace 1
