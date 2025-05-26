1da456e170e8a105254b9e93b766b28bc12433d7
titembaatar <etienne.guillemin@protonmail.com>
2025-05-27 01:54:05 +0200

    better skip if installed

--- runs/nvim.sh
+++ runs/nvim.sh
@@ -4,13 +4,12 @@ source "$HOME"/.dotfiles/pkg
 
 src_dir="$HOME/src/neovim"
 
-install ninja-build cmake gcc make gettext curl glibc-gconv-extra
-
 if [ -d "$src_dir" ]; then
   log_info "neovim already installed."
   exit 0
 fi
 
+install ninja-build cmake gcc make gettext curl glibc-gconv-extra
 mkdir -p "$src_dir"
 git clone https://github.com/neovim/neovim "$src_dir"
 cd "$src_dir"
--- runs/ohmyposh.sh
+++ runs/ohmyposh.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -e
+source $HOME/.dotfiles/pkg
+
+if [ -f $HOME/.local/bin/oh-my-posh ];then
+  log_info "oh my posh already installed. Skipping."
+  exit 0
+fi
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
