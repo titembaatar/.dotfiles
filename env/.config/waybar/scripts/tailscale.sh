035b841d8a1596c0658b87ca18d98fc9503007b9
titembaatar <etienne.guillemin@protonmail.com>
2026-01-24 01:40:23 +0100

    waybar/tailscale: refactor

--- env/.config/waybar/scripts/tailscale.sh
+++ env/.config/waybar/scripts/tailscale.sh
@@ -1,44 +1,51 @@
 #!/usr/bin/env bash
 
-declare connected text class tooltip
+connected=false
+text="󰦞"
+class="disconnected"
+tooltip="Not on the tailnet."
+
 get_tailscale_status() {
-  connected=false
-  text="󰦞"
-  class="disconnected"
-  tooltip="Not on the tailnet."
+	local status
 
-  local status_json
-  if status_json=$(tailscale status --json 2>/dev/null); then
-    local backend_state
-    backend_state=$(echo "$status_json" | jq -r '.BackendState // "NoState"')
-    tailnet_ip=$(echo "$status_json" | jq -r '.Self.TailscaleIPs[0]')
+	if ! status=$(tailscale status --json 2>/dev/null); then
+		return 1
+	fi
 
-    if [[ "$backend_state" == "Running" ]]; then
-      connected=true
-      text=""
-      class="connected"
-      tooltip="Connected to the tailnet.\nIP: $tailnet_ip"
-    fi
-  fi
+	local data tailnet_ip
+	data=$(jq -r '.BackendState + "\t" + .Self.TailscaleIPs[0]' \
+		<<< "$status")
+
+	local backend_state="${data%$'\t'*}"
+	local tailnet_ip="${data#*$'\t'}"
+
+	if [[ "$backend_state" == "Running" ]]; then
+		connected=true
+		text=""
+		class="connected"
+		tooltip="Connected to the tailnet.\nIP: $tailnet_ip"
+	fi
 }
 
 case "$1" in
-  --status)
-    get_tailscale_status
+--status)
+	get_tailscale_status
 
-    echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"class\": \"$class\"}"
-    ;;
-  --toggle)
-    get_tailscale_status
+	printf '{"text": "%s", "tooltip": "%s", "class": "%s"}' \
+		"$text" "$tooltip" "$class"
+	;;
+--toggle)
+	get_tailscale_status
 
-    if $connected; then
-      sudo tailscale down
-    else
-      sudo tailscale up
-    fi
-    ;;
-  *)
-    echo "usage: $0 {--status|--toggle}"
-    exit 1
-    ;;
+	if $connected; then
+		sudo tailscale down
+		exit 0
+	fi
+
+	sudo tailscale up
+	;;
+*)
+	echo "usage: $0 {--status|--toggle}"
+	exit 1
+	;;
 esac
