8a857ff3e3b425738a00ad237ad9785e328ef71c
titembaatar <etienne.guillemin@protonmail.com>
2026-01-24 01:40:07 +0100

    waybar/pulseaudio: refactor

--- env/.config/waybar/scripts/pulseaudio.sh
+++ env/.config/waybar/scripts/pulseaudio.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
-set -e
 
-running=$(pactl list short sinks | grep RUNNING | awk '{print $2}')
+running=$(pactl get-default-sink)
+
+is_bluetooth=$(bluetoothctl devices Connected)
+
+if [[ -n $is_bluetooth ]]; then
+	bt_mac=${is_bluetooth#* }
+	bt_mac=${bt_mac%% *}
+	bt_sink="bluez_output.$bt_mac"
+fi
+
 headphones="alsa_output.usb-Generic_USB_Audio-00.HiFi__Speaker__sink"
 speakers="alsa_output.usb-Generic_USB_Audio-00.HiFi__SPDIF__sink"
-bluetooth="bluez_output.18_9C_2C_B2_FF_67.1"
 
-mac="18:9C:2C:B2:FF:67"
+ear_device=${bt_sink:-$headphones}
 
-if [ $running = $speakers ] && bluetoothctl info "$mac" | grep -q "Connected: yes"; then
-  pactl set-default-sink $bluetooth
-elif [ $running = $speakers ]; then
-  pactl set-default-sink $headphones
-else
-  pactl set-default-sink $speakers
+if [[ $running = "$speakers" ]]; then
+	pactl set-default-sink "$ear_device"
+	exit 0
 fi
+
+pactl set-default-sink "$speakers"
