bf2ec9b0cd99bc0ca5074c34b35a753e4552e7e4
titembaatar <etienne@titem.top>
2026-02-23 01:46:38 +0100

    runs/iosevka: cleanup

--- runs/build_iosevka.sh
+++ runs/build_iosevka.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-INSTALL_CMD=$1
-CUSTOM_IOSEVKA_CONFIG=$2
-font_dir=$3
+CUSTOM_IOSEVKA_CONFIG=$1
+FONT_DIR=$2
 
-[[ -z $font_dir ]] && exit 1
-mkdir -p "$font_dir"
+[[ -z $FONT_DIR ]] && exit 1
+mkdir -p "$FONT_DIR"
 
 tmp_dir="$(mktemp -d)"
 trap 'rm -rf "$tmp_dir"' EXIT
@@ -19,10 +18,8 @@ unzip -qo "$tmp_dir/tmp.zip" -d "$tmp_dir"
 
 git clone --depth 1 https://github.com/be5invis/Iosevka.git "$tmp_iosevka"
 
-$INSTALL_CMD nodejs npm ttfautohint fontforge python3 wget unzip
-
+cp -f "$CUSTOM_IOSEVKA_CONFIG" "$tmp_iosevka"
 cd "$tmp_iosevka" || exit 1
-cp -f "$CUSTOM_IOSEVKA_CONFIG" ./
 npm install
 if ! npm run build -- ttf::Iosevka --jCmd=16; then
 	echo "Iosevka font generation failed"
@@ -30,13 +27,11 @@ if ! npm run build -- ttf::Iosevka --jCmd=16; then
 fi
 
 while IFS= read -r -d '' ttf; do
-	echo "Patching $ttf..."
 	t=${ttf#*Iosevka/}
-	t=${t%/*.ttf}
-	tmp_ttf="$tmp_font/$t"
+	tmp_ttf="$tmp_font/${t%/*.ttf}"
 	fontforge -script "$tmp_dir/font-patcher" --careful --quiet \
 		--variable-width-glyphs --complete --adjust-line-height \
 		--outputdir "$tmp_ttf" "$ttf"
 done < <(find "$tmp_iosevka/dist/Iosevka" -name "*.ttf" -print0)
 
-cp -r "$tmp_font/TTF/"* "$font_dir/"
+cp -r "$tmp_font"/TTF/* "$FONT_DIR"
